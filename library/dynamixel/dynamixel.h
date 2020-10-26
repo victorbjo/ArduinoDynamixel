00a1b48bb5f95ddb79f08d93a6e577991f16ed23
Victor Bjoerholm <47253754+victor@fridayhome.net>
2020-10-26 16:15:39 +0100

    Library can now do the most basic/important things. Libtest shows all commands

--- library/dynamixel/dynamixel.h
+++ library/dynamixel/dynamixel.h
@@ -1,1 +1,29 @@
-C:/Users/victo/Documents/Arduino/libraries/dynamixel/dynamixel.h
+#ifndef Morse_h
+#define Morse_h
+
+#include "Arduino.h"
+
+class dynamixel
+{
+  public:
+    dynamixel(uint8_t id);
+    void dot();
+    void dash();
+    void action();
+    void ledOn();
+    void ledOff();
+    void torgueEnable();
+    void torgueDisable();
+    void goalVel(int velocity);
+    void sendPckg(uint8_t msg[], int msgSize);
+    void changeMode(int mode);
+    void goalPos(uint16_t pos);
+    void velLimit(uint16_t limit);
+    void pwmLimit(uint16_t limit);
+    uint16_t compute_crc(uint16_t crc_accum, uint8_t* data_blk_ptr, size_t data_blk_size);
+  private:
+    uint8_t _id;
+    uint16_t  crc_table[256];
+};
+
+#endif
